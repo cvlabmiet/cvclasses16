b5294aef260d851e345b353726296c2950a61b20
LenaBuranova <alenaburanova.93@gmail.com>
2016-10-22 10:52:51 +0300

    дз 3

--- lesson3/main.cpp
+++ lesson3/main.cpp
@@ -4,7 +4,7 @@
 ///@Date: 08 September 2015
 
 #include "stdafx.h"
-
+#include <fstream>
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -17,6 +17,11 @@
 const std::string c_origWindowName = "Original Picture";
 const std::string c_edgeWindowName = "Edge Picture";
 
+cv::Mat copyImgEdge;
+std::string nameCopyImgEdge;
+std::ofstream myfile;
+
+
 int g_threshold = 50;
 int g_operatorId = 0;
 std::pair<int, int> g_lastThOp = { -1, -1 };
@@ -24,9 +29,9 @@ std::pair<int, int> g_lastThOp = { -1, -1 };
 ///@brief Edge operator specification
 struct EdgeOperator
 {
-   std::string Name;
-   float fx[3*3]; // kernel for horizontal derivation
-   float fy[3*3]; // kernel for vertical derivation
+	std::string Name;
+	float fx[3 * 3]; // kernel for horizontal derivation
+	float fy[3 * 3]; // kernel for vertical derivation
 };
 
 const std::string LoGName = "LoG";
@@ -34,21 +39,75 @@ const std::string CannyName = "Canny";
 
 ///@brief array of edge operators
 std::vector<EdgeOperator> g_operatorSpec = {
-   EdgeOperator{ "Robert\'s",       { -1,  1, 0 , 0, 0, 0, 0, 0, 0 },
-                                    { -1,  0, 0,  1, 0, 0, 0, 0, 0 } },
-   EdgeOperator{ "Robert\'s Cross", { -1,  0, 0 , 0, 1, 0, 0, 0, 0 },
-                                    {  0, -1, 0,  1, 0, 0, 0, 0, 0 } },
-   EdgeOperator{ "Prewitt",         { -1,  0,  1, -1, 0, 1, -1, 0, 1 },
-                                    { -1, -1, -1,  0, 0, 0,  1, 1, 1 } },
-   EdgeOperator{ "Sobel",           { -1,  0,  1, -2, 0, 2, -1, 0, 1 },
-                                    { -1, -2, -1,  0, 0, 0,  1, 2, 1 } },
-   EdgeOperator{ "Scharr",          { -3,   0,  3, -10, 0, 10, -3,  0, 3},
-                                    { -3, -10, -3,   0, 0,  0,  3, 10, 3} },
-   EdgeOperator{ LoGName,           { 1,  1, 1 , 1, -8, 1, 1, 1, 1 },
-                                    { 0,  0, 0,  0,  0, 0, 0, 0, 0 } },
-   EdgeOperator{ CannyName },
+	EdgeOperator{ "LoG7" },
+	EdgeOperator{ "Robert\'s", { -1, 1, 0, 0, 0, 0, 0, 0, 0 },
+	{ -1, 0, 0, 1, 0, 0, 0, 0, 0 } },
+	EdgeOperator{ "Robert\'s Cross", { -1, 0, 0, 0, 1, 0, 0, 0, 0 },
+	{ 0, -1, 0, 1, 0, 0, 0, 0, 0 } },
+	EdgeOperator{ "Prewitt", { -1, 0, 1, -1, 0, 1, -1, 0, 1 },
+	{ -1, -1, -1, 0, 0, 0, 1, 1, 1 } },
+	EdgeOperator{ "Prewitt diagonal", { 0, 1, 1, -1, 0, 1, -1, -1, 0 },
+	{ -1, -1, 0, -1, 0, 1, 0, 1, 1 } },
+	EdgeOperator{ "Sobel", { -1, 0, 1, -2, 0, 2, -1, 0, 1 },
+	{ -1, -2, -1, 0, 0, 0, 1, 2, 1 } },
+	EdgeOperator{ "Scharr", { -3, 0, 3, -10, 0, 10, -3, 0, 3 },
+	{ -3, -10, -3, 0, 0, 0, 3, 10, 3 } },
+	EdgeOperator{ LoGName, { 1, 1, 1, 1, -8, 1, 1, 1, 1 },
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0 } },
+	EdgeOperator{ CannyName },
+	
 };
 
+
+/////////////////////////////////////////////////////////////////////
+static void onMouse(int event, int x, int y, int, void*)
+{
+	
+	if (event == CV_EVENT_LBUTTONUP){
+		cv::Mat edgesTrue = cv::imread("museumofpushkin_contour.png", 0);
+
+		if (edgesTrue.empty())
+		{
+			std::cout << "can't open file" << std::endl;
+		}
+
+		const std::string nameImage = nameCopyImgEdge + ".bmp";
+		cv::imwrite(nameImage, copyImgEdge);
+
+		{
+			//истинно положительные (выбранные)
+			double numTruePos = 0;
+			//все истинные 
+			double numTrue = 0;
+			// все найденные
+			double numFound = 0;
+			double recall = 0;
+			double precision = 0;
+			copyImgEdge.convertTo(copyImgEdge, CV_8UC1);
+			for (int i = 0; i < edgesTrue.rows; ++i)
+			{
+				for (int j = 0; j < edgesTrue.cols; ++j)
+				{
+					if (edgesTrue.at<uchar>(i, j) > 0){
+						numTrue++;
+					}
+					if (copyImgEdge.at<uchar>(i, j) > 0){
+						numFound++;
+					}
+					if (edgesTrue.at<uchar>(i, j) > 0 && copyImgEdge.at<uchar>(i, j) > 0){
+						numTruePos++;
+					}
+				}
+			}
+			recall = numTruePos / numTrue;
+			precision = numTruePos / numFound;
+			std::cout << "recall = " << recall << std::endl;
+			std::cout << "precision = " << precision << std::endl;
+			myfile << recall <<" "<<precision << std::endl;
+		}
+	}
+		
+}
 /////////////////////////////////////////////////////////////////////
 void apply1stDerivativeAlgo(cv::Mat& i_img, EdgeOperator& i_oper, cv::Mat& o_edges)
 {
@@ -64,14 +123,28 @@ void apply1stDerivativeAlgo(cv::Mat& i_img, EdgeOperator& i_oper, cv::Mat& o_edg
    double maxVal = 0;
    cv::minMaxLoc(imgMag, nullptr, &maxVal);
 
-   cv::threshold(imgMag, o_edges, 0.01 * g_threshold * maxVal, 1, CV_THRESH_BINARY);
+   cv::threshold(imgMag, o_edges, 0.01 * g_threshold * maxVal, 255, CV_THRESH_BINARY);
+//   double recall, precision;
+  // cv::Mat imgTrueEdge = cv::imread("C:\\Users\\Elena\\Pictures\\музей_пушкина_контур_new.png", 1);
 }
 
 /////////////////////////////////////////////////////////////////////
 void apply2ndDerivativeAlgo(cv::Mat& i_img, EdgeOperator& i_oper, cv::Mat& o_edges)
 {
    cv::Mat log;
-   cv::filter2D(i_img, log, CV_32F, cv::Mat(3, 3, CV_32F, i_oper.fx));
+   if (i_oper.Name == "LoG7"){
+	   float i_oper_fx[7 * 7] = { 1.96518760e-05, 2.39385216e-04, 1.07183544e-03, 1.76497071e-03, 1.07183544e-03, 2.39385216e-04, 1.96518760e-05,
+		   2.39385216e-04, 2.90207827e-03, 1.23955399e-02, 1.91204498e-02, 1.23955399e-02, 2.90207827e-03, 2.39385216e-04,
+		   1.07183544e-03, 1.23955399e-02, 2.71411229e-02, -1.64952149e-02, 2.71411229e-02, 1.23955399e-02, 1.07183544e-03,
+		   1.76497071e-03, 1.91204498e-02, -1.64952149e-02, -2.47466319e-01, -1.64952149e-02, 1.91204498e-02, 1.76497071e-03,
+		   1.07183544e-03, 1.23955399e-02, 2.71411229e-02, -1.64952149e-02, 2.71411229e-02, 1.23955399e-02, 1.07183544e-03,
+		   2.39385216e-04, 2.90207827e-03, 1.23955399e-02, 1.91204498e-02, 1.23955399e-02, 2.90207827e-03, 2.39385216e-04,
+		   1.96518760e-05, 2.39385216e-04, 1.07183544e-03, 1.76497071e-03, 1.07183544e-03, 2.39385216e-04, 1.96518760e-05 };
+	   cv::filter2D(i_img, log, CV_32F, cv::Mat(7, 7, CV_32F, i_oper_fx));	  
+   }
+   else{
+	   cv::filter2D(i_img, log, CV_32F, cv::Mat(3, 3, CV_32F, i_oper.fx));
+   }
 
    double minVal = 0;
    double maxVal = 0;
@@ -85,13 +158,12 @@ void apply2ndDerivativeAlgo(cv::Mat& i_img, EdgeOperator& i_oper, cv::Mat& o_edg
       for (int y = 1; y < log.rows - 1; ++y)
       {
          const cv::Mat nghb = log.colRange({ x - 1, x + 2 }).rowRange({ y - 1, y + 2 });
-
          if (nghb.at<float>(0, 0) * nghb.at<float>(2, 2) < 0 && std::abs(nghb.at<float>(0, 0) - nghb.at<float>(2, 2)) > th ||
             nghb.at<float>(1, 0) * nghb.at<float>(1, 2) < 0 && std::abs(nghb.at<float>(1, 0) - nghb.at<float>(1, 2)) > th ||
             nghb.at<float>(2, 0) * nghb.at<float>(0, 2) < 0 && std::abs(nghb.at<float>(2, 0) - nghb.at<float>(0, 2)) > th ||
             nghb.at<float>(2, 1) * nghb.at<float>(0, 1) < 0 && std::abs(nghb.at<float>(2, 1) - nghb.at<float>(0, 1)) > th)
          {
-            o_edges.at<float>(y, x) = 1.0;
+            o_edges.at<float>(y, x) = 255;
          }
       }
    }
@@ -119,7 +191,7 @@ void processFrame(cv::Mat& i_image)
    {
       cv::Canny(gray, edges, g_threshold, g_threshold / 2);
    }
-   else if(oper.Name == LoGName)
+   else if (oper.Name == LoGName || oper.Name == "LoG7")
    {
       apply2ndDerivativeAlgo(gray, oper, edges);
    }
@@ -127,15 +199,17 @@ void processFrame(cv::Mat& i_image)
    {
       apply1stDerivativeAlgo(gray, oper, edges);
    }
-
+   edges.copyTo(copyImgEdge);
    const int bufSize = 100;
    char chint[bufSize];
    sprintf_s(chint, bufSize, "%s Operator, %d", oper.Name.data(), g_threshold);
    std::string shint{ chint };
+   nameCopyImgEdge = shint;
    const auto txtSize = cv::getTextSize(shint, CV_FONT_HERSHEY_PLAIN, 1.0, 1, nullptr);
    cv::putText(edges, shint, { 10, 10 + txtSize.height }, CV_FONT_HERSHEY_PLAIN, 1.0, 1.0);
 
    cv::imshow(c_edgeWindowName, edges);
+
 }
 
 ///@brief provides video capturing from the camera and frame processing
@@ -156,23 +230,29 @@ void processVideo()
 	}
 }
 
+
+
+
 ///@brief provides image file processing
 bool processImage(const std::string& i_name)
 {
 
-   cv::Mat img0 = cv::imread(i_name, 1);
+	cv::Mat img0 = cv::imread(i_name, 1);
    if (img0.empty())
    {
       std::cerr << "Couldn'g open image " << i_name << ". Usage: lesson3 <image_name>\n";
       return false;
    }
-
+   
+   myfile.open("re_pre_prewitt.txt");
    while (cv::waitKey(1) < 0)
    {
       cv::Mat frame;
       processFrame(img0);
+	  cv::setMouseCallback(c_edgeWindowName, onMouse, 0);
+	  
    }
-
+   myfile.close();
    return true;
 }
 
@@ -211,3 +291,5 @@ int _tmain(int argc, char** argv)
 
 	return 0;
 }
+
+
